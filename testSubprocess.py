8cafc8e0494d4cf9647a6d5619976cf794ef49a9
milorads <milorad.sekulovic@gmail.com>
2017-03-29 13:33:02 +0000

    added refresh of the arp table every 30 sec

--- testSubprocess.py
+++ testSubprocess.py
@@ -1,6 +1,7 @@
 import time
 import re
 import subprocess
+counter = 1;
 while 1:
     arp_out = subprocess.check_output(['arp','-i','wlan0'])
 
@@ -11,5 +12,10 @@ while 1:
     print re_out
 
     #sudo ip -s -s neigh flush all
+    #subprocess.call('sudo ip -s -s neigh flush all', shell=True)
+    #flushing might not work perfectly so trying to flush every 30 sec
+    if (counter%6 == 0):
+        subprocess.call('sudo ip -s -s neigh flush all', shell=True)
 
+    counter+=1
     time.sleep(5)
