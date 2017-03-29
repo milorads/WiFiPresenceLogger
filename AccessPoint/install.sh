523b98800389198b40e57263ab1efa0755c4c165
milorads <milorad.sekulovic@gmail.com>
2017-03-29 14:19:17 +0000

    added check to see if installed in order to avoid adding iptables refresh on boot on multiple script run

--- AccessPoint/install.sh
+++ AccessPoint/install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+INSTALLED=$(cat ./installed)
 echo "Instalacija potrebnog softvera ( hostapd & dnsmasq )"
 sudo apt-get install hostapd dnsmasq -y
 
@@ -25,9 +26,14 @@ sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 sudo iptables -A FORWARD -i eth0 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT
 sudo iptables -A FORWARD -i wlan0 -o eth0 -j ACCEPT
 
-echo "Aktivacija povracaja IP adresa na reboot-u"
-sudo sed -i -e '$i iptables-restore < /etc/iptables.ipv4.nat  \n' /etc/rc.local
+if [ $INSTALLED = 0]; then
+	echo "Aktivacija povracaja IP adresa na reboot-u"
+	sudo sed -i -e '$i iptables-restore < /etc/iptables.ipv4.nat  \n' /etc/rc.local
 
 echo "Pokretanje servisa"
 sudo service hostapd start
 sudo service dnsmasq start
+
+echo '1' > ./installed
+
+
---
+++ AccessPoint/test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "testing"
+SUM=$(cat ./installed)
+
+echo $SUM
