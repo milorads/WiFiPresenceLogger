a9e260ff728abafe4dd08e705713eb2da8620ef9
milorads <milorad.sekulovic@gmail.com>
2017-03-29 11:09:30 +0000

    added loggers of the connected ip adderesses -> commented arp clean code

---
+++ testSubprocess.py
@@ -0,0 +1,15 @@
+import time
+import re
+import subprocess
+while 1:
+    arp_out = subprocess.check_output(['arp','-i','wlan0'])
+
+    print arp_out
+
+    re_out = re.findall(r"((\w{2,2}\:{0,1}){6})", arp_out)
+
+    print re_out
+
+    #sudo ip -s -s neigh flush all
+
+    time.sleep(5)
