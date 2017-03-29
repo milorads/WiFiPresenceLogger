b7b5399ab719838ae8ccbc6b65176e0302ba0830
milorads <milorad.sekulovic@gmail.com>
2017-03-29 09:43:11 +0000

    finished install script

--- AccessPoint/install.sh
+++ AccessPoint/install.sh
@@ -11,8 +11,19 @@ sudo cp ./interfaces /etc/network/interfaces
 sudo service dhcpd restart
 sudo ifdown wlan0; sudo ifup wlan0
 
-
-
 echo "Kopiranje konfiguracionih fajlova"
 sudo cp ./hostapd.conf /etc/hostapd.conf
+sudo cp ./hostapd /etc/default/hostapd
 sudo cp ./dnsmasq.conf /etc/dnsmasq.conf
+
+echo "Aktiviranje prosledjivanja interneta - ukoliko je prikacen ethernet-om"
+echo "net.ipv4.ip_forward=1" | sudo tee /etc/sysctl.conf
+sudo sh -c "echo 1> /proc/sys/net/ipv4/ip_forward"
+
+echo "Aktiviranje prosledjivanja interneta - enth->wlan0"
+sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+sudo iptables -A FORWARD -i eth0 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+sudo iptables -A FORWARD -i wlan0 -o eth0 -j ACCEPT
+
+echo "Aktivacija povracaja IP adresa na reboot-u"
+sudo sed -i -e '$i iptables-restore < /etc/iptables.ipv4.nat  \n' /etc/rc.local
