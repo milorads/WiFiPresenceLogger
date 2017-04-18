ba5dccaf4c49707a591fea5620e867d724218ad7
milorads <milorad.sekulovic@gmail.com>
2017-04-18 10:46:39 +0000

    finished script todo:test

--- AccessPoint2/install.sh
+++ AccessPoint2/install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 INSTALLED=$(cat ./installed)
-echo "Instalacija potrebnog softvera ( lighttpd, hostapd & dnsmasq )"
+echo "Instalacija potrebnog softvera"
+echo "lighttpd"
 sudo apt-get update
 sudo apt-get install lighttpd
 
@@ -9,6 +10,7 @@ sudo chown www-data:www-data /var/www
 sudo chmod 775 /var/www
 sudo usermod -a -G www-data pi
 
+echo "hostapd"
 sudo apt-get install hostapd
 sudo cp ./interfaces /etc/network/interfaces
 sudo cp ./hostapd.conf /etc/hostapd/hostapd.conf
@@ -17,9 +19,17 @@ sudo cp ./hostapd /etc/default/hostapd
 sudo service hostapd start
 sudo service hostapd stop
 
+echo "dnsmasq"
 sudo apt-get install dnsmasq
 
 sudo service dnsmasq start
 sudo service dnsmasq restart
 
-echo "172.26.1.1	eins" | sudo tee /etc/hosts
+
+if [ $INSTALLED = 0 ] ; then
+	echo "changing hosts"
+	echo "172.26.1.1	eins" | sudo tee /etc/hosts
+fi
+
+echo "rebooting..."
+sudo reboot
