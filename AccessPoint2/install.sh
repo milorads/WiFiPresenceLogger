645650d6225642b68ebc5708599f1202d95d4634
milorads <milorad.sekulovic@gmail.com>
2017-04-18 09:44:37 +0000

    writing new ap settings to test caprive portal function (with and without internet from eth)

---
+++ AccessPoint2/install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+INSTALLED=$(cat ./installed)
+echo "Instalacija potrebnog softvera ( lighttpd, hostapd & dnsmasq )"
+sudo apt-get update
+sudo apt-get install lighttpd
+
+sudo chown www-data:www-data /var/www
+sudo chmod 775 /var/www
+sudo usermod -a -G www-data pi
+
