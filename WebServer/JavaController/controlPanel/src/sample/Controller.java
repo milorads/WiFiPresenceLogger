363a352dae4e2317811dc573d28ad64e946375e4
milorads <milorad.sekulovic@gmail.com>
2017-05-25 13:15:42 +0200

    ran node js server from java app

--- WebServer/JavaController/controlPanel/src/sample/Controller.java
+++ WebServer/JavaController/controlPanel/src/sample/Controller.java
@@ -2,6 +2,8 @@ package sample;
 
 import javafx.fxml.FXML;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 
 class Home{
@@ -11,20 +13,41 @@ class Home{
 public class Controller extends Home{
 
     @FXML
-    public void Runner(){
+    public void Runner() throws IOException {
 //        ProcessBuilder b = new ProcessBuilder("node "+Home+"api.js");
 //        try {
 //            b.start();
 //        } catch (IOException e) {
 //            e.printStackTrace();
 //        }
-        Runtime rt = Runtime.getRuntime();
-        String[] commands = {"node "+Home+"api.js"};
-        Process proc;
-        try {
-            proc = rt.exec(commands);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+
+//        Runtime rt = Runtime.getRuntime();
+//        String[] commands = {"node ./serviceApp/api.js"};
+//        Process proc;
+//        try {
+//            proc = rt.exec(commands);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
+        String[] args = new String[] {"/bin/bash", "-c", "node ./serviceApp/api.js"};
+        Process proc = new ProcessBuilder(args).start();
+
+//        BufferedReader br = new BufferedReader(new FileReader("./serviceApp/api.js"));
+//        try {
+//            StringBuilder sb = new StringBuilder();
+//            String line = br.readLine();
+//
+//            while (line != null) {
+//                sb.append(line);
+//                sb.append(System.lineSeparator());
+//                line = br.readLine();
+//            }
+//            String everything = sb.toString();
+//            String a = "";
+//        } finally {
+//            br.close();
+//        }
+
     }
 }
