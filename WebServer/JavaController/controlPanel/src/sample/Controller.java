d17f1d1fa1228a6dc5195a59c44c8ee7f430fd80
milorads <milorad.sekulovic@gmail.com>
2017-05-25 12:41:28 +0200

    added buttons

--- WebServer/JavaController/controlPanel/src/sample/Controller.java
+++ WebServer/JavaController/controlPanel/src/sample/Controller.java
@@ -1,5 +1,11 @@
 package sample;
 
+import javafx.fxml.FXML;
+
 public class Controller {
 
+    @FXML
+    public void Runner(){
+        ProcessBuilder b = new ProcessBuilder("node mynodejs.js", "-args");
+    }
 }
--- WebServer/JavaController/controlPanel/src/sample/Main.java
+++ WebServer/JavaController/controlPanel/src/sample/Main.java
@@ -1,6 +1,7 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -15,6 +16,7 @@ public class Main extends Application {
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
+
 
 
     public static void main(String[] args) {
