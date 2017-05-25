223f2955bebefb7aa6c48ce259fa1e737cc0bb0c
milorads <milorad.sekulovic@gmail.com>
2017-05-25 13:06:01 +0200

    moved node server into java folder

---
+++ WebServer/JavaController/controlPanel/serviceApp/dbcommands.js
@@ -0,0 +1,17 @@
+//Load modules
+var sqlite3         =       require('sqlite3').verbose();
+var db              =       new sqlite3.Database('./database_name.db');
+
+//Perform SELECT Operation
+db.all("SELECT * from blah blah blah where this="+that,function(err,rows){
+//rows contain values while errors, well you can figure out.
+});
+
+//Perform INSERT operation.
+db.run("INSERT into table_name(col1,col2,col3) VALUES (val1,val2,val3)");
+
+//Perform DELETE operation
+db.run("DELETE * from table_name where condition");
+
+//Perform UPDATE operation
+db.run("UPDATE table_name where condition");
--- WebServer/JavaController/controlPanel/src/sample/Controller.java
+++ WebServer/JavaController/controlPanel/src/sample/Controller.java
@@ -2,10 +2,29 @@ package sample;
 
 import javafx.fxml.FXML;
 
-public class Controller {
+import java.io.IOException;
+
+class Home{
+    public static String Home = "~/Documents/WiFresenceLogger/WebServer/serviceApp/";
+}
+
+public class Controller extends Home{
 
     @FXML
     public void Runner(){
-        ProcessBuilder b = new ProcessBuilder("node mynodejs.js", "-args");
+//        ProcessBuilder b = new ProcessBuilder("node "+Home+"api.js");
+//        try {
+//            b.start();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+        Runtime rt = Runtime.getRuntime();
+        String[] commands = {"node "+Home+"api.js"};
+        Process proc;
+        try {
+            proc = rt.exec(commands);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
