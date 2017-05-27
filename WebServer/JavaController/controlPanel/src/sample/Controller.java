01c3c7911611a5a6849510330a1ece16638ca22c
milorads <milorad.sekulovic@gmail.com>
2017-05-27 13:12:23 +0200

    added jdbc sqlite driver and test connector method

--- WebServer/JavaController/controlPanel/src/sample/Controller.java
+++ WebServer/JavaController/controlPanel/src/sample/Controller.java
@@ -1,11 +1,38 @@
 package sample;
 
 import javafx.fxml.FXML;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Controller {
 
     @FXML
     public void Runner(){
         ProcessBuilder b = new ProcessBuilder("node mynodejs.js", "-args");
+    }
+
+    @FXML
+    public void JDBCTester(){
+        Connection conn = null;
+        try {
+            // db parameters
+            String url = "jdbc:sqlite:mydb.db";
+            // create a connection to the database
+            conn = DriverManager.getConnection(url);
+
+            System.out.println("Connection to SQLite has been established.");
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+    }
     }
 }
