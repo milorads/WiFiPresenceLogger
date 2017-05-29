007d45ddd43268ff0e0293617b745931f24c6ca7
milorads <milorad.sekulovic@gmail.com>
2017-05-29 13:47:23 +0200

    implemented search function

--- WebServer/finalController/src/finalControl/BL/PDatabaseHandler.java
+++ WebServer/finalController/src/finalControl/BL/PDatabaseHandler.java
@@ -107,7 +107,27 @@ public class PDatabaseHandler implements IDatabase, IPermanentDatabase {
 
     @Override
     public PDBModel Search(Mac mac) {
-        return null;
+        String sql = "SELECT Id, Mac, Index FROM users WHERE Mac = ?";
+        try(Connection conn = this.connect();
+            PreparedStatement pstatement = conn.prepareStatement(sql))
+        {
+            pstatement.setString(1, mac.Get());
+            PDBModel mod = new PDBModel();
+            ResultSet rs = pstatement.executeQuery();
+            if(rs.next()){
+                mod.setId(rs.getInt("Id"));
+                mod.setMac(new Mac(rs.getString("Mac")));
+                mod.setIndex(rs.getString("Index"));
+            }
+            return mod;
+        }
+        catch (SQLException e) {
+            System.out.println(e.getMessage());
+            return null;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
     }
 
     private Connection connect(){
