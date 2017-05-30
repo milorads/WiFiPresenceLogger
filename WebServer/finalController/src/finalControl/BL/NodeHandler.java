fafa5e9c9962a3c8d1a530764fcd26c825b2f75c
milorads <milorad.sekulovic@gmail.com>
2017-05-30 16:29:53 +0200

    added parameters to be passes and enums

--- WebServer/finalController/src/finalControl/BL/NodeHandler.java
+++ WebServer/finalController/src/finalControl/BL/NodeHandler.java
@@ -1,7 +1,15 @@
 package finalControl.BL;
 
+import java.io.IOException;
+
 /**
  * Created by milor on 28.5.2017..
  */
 public class NodeHandler {
+
+    private void NodeLogic() throws IOException {
+        String[] args = new String[] {"/bin/bash", "-c", "node ./nodeWebServer/finalTest.js"};
+            Process proc = new ProcessBuilder(args).start();
+
+    }
 }
--- WebServer/finalController/src/finalControl/BL/ThreadController.java
+++ WebServer/finalController/src/finalControl/BL/ThreadController.java
@@ -1,8 +1,23 @@
 package finalControl.BL;
 
+import java.util.Dictionary;
+
 /**
  * Created by milor on 29.5.2017..
  */
 public class ThreadController {
     // from here Python and Node Handlers will be run into two separate threads
+
+    public enum Parameters{
+        clsLen,
+        numOfCls,
+        breakLen,
+        clsName,
+        faculty,
+        yr,
+        professorName
+    }
+
+    public void StartLogging(Dictionary<String,String> dict){}
+
 }
