f2411e96545ece985ca9c916559608abb2ca32cc
milorads <milorad.sekulovic@gmail.com>
2017-12-15 08:45:06 +0100

    forgotten commit

--- WebServer/finalController/src/finalControl/Models/Mac.java
+++ WebServer/finalController/src/finalControl/Models/Mac.java
@@ -12,8 +12,8 @@ public class Mac {
         if (macParts.length >= 0) {
             if (macParts.length == 5) {//valid mac
                 boolean ind = true;
-                for (String mac : macParts) {
-                    if(mac.length() != 2){ind = false;}
+                for (String mPart : macParts) {
+                    if(mPart.length() != 2){ind = false;}
                 }
                 if(!ind){
                     throw new Exception("Not valid mac");
