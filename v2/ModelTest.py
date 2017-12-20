b9d071ba480d3ad41a0f658626efef83c07449e5
milorads <milorad.sekulovic@gmail.com>
2017-12-20 12:14:05 +0100

    indentation changed

--- v2/ModelTest.py
+++ v2/ModelTest.py
@@ -1,36 +1,31 @@
 class ArpModel(object):
-
-	"""Attributes:
-		hostName: name of the device that is connected to the ap
-		ipAddress: ip address given to the device connected
-		macAddress: physical address of the connected device
-	"""
-
-    def __init__(ip, mac, host = "Unknown"):
-		"""Constructor with host name defaulted to Unknown in case it cannot be parsed""""
+    #Attributes:
+    #hostName: name of the device that is connected to the ap
+    #ipAddress: ip address given to the device connected
+    #macAddress: physical address of the connected device
+    def __init__(self, ip, mac, host = "Unknown"):
+        #Constructor with host name defaulted to Unknown in case it cannot be parsed
         self.hostName = host
         self.ipAddress = ip
         self.macAddress = mac
-		
-	def GetIp(self):
-		return self.ipAddress
-	
-	def GetMac(self):
-		return self.macAddress
-		
-	def GetHost(self):
-		return self.hostName
-
-def checkBase(listOfModels):
-	for model in listOfModels:
-		print item.GetIp
-		print item.GetMac
-		print item.GetHost
+    def GetIp(self):
+	return (self.ipAddress)
+    def GetMac(self):
+	return (self.macAddress)
+    def GetHost(self):
+	return (self.hostName)
+    pass
 
 modeli = {
     ArpModel("ip1","mac1","host1"),
     ArpModel("ip2","mac2","host1"),
-    ArpModel("ip3","mac3"),	
+    ArpModel("ip3","mac3")
 }
+
+def checkBase(listOfModels):
+    for model in listOfModels:
+        print model.GetIp()
+        print model.GetMac()
+	print model.GetHost()
 
 checkBase(modeli)
