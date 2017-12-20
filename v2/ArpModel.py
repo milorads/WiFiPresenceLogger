6ba65fa98fdc020ebf9ae92faf60a94a7075f960
milorads <milorad.sekulovic@gmail.com>
2017-12-20 10:57:27 +0100

    added arp model

---
+++ v2/ArpModel.py
@@ -0,0 +1,19 @@
+class ArpModel(object):
+
+	"""Attributes:
+		hostName: name of the device that is connected to the ap
+		ipAddress: ip address given to the device connected
+		macAddress: physical address of the connected device
+	"""
+
+    def __init__(ip, mac, host = "Unknown"):
+		"""Constructor with host name defaulted to Unknown in case it cannot be parsed""""
+        self.hostName = host
+        self.ipAddress = ip
+        self.macAddress = mac
+		
+	def GetIp(self):
+		return self.ipAddress
+	
+	def GetMac(self):
+		return self.macAddress
