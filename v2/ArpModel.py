180f9fd3f4e8b78638979d7b64ce37a1f85f7fa5
milorads <milorad.sekulovic@gmail.com>
2017-12-20 12:14:29 +0100

    model indentation changed

--- v2/ArpModel.py
+++ v2/ArpModel.py
@@ -1,19 +1,17 @@
 class ArpModel(object):
-
-	"""Attributes:
-		hostName: name of the device that is connected to the ap
-		ipAddress: ip address given to the device connected
-		macAddress: physical address of the connected device
-	"""
-
-    def __init__(ip, mac, host = "Unknown"):
-		"""Constructor with host name defaulted to Unknown in case it cannot be parsed""""
+    #Attributes:
+    #hostName: name of the device that is connected to the ap
+    #ipAddress: ip address given to the device connected
+    #macAddress: physical address of the connected device
+    def __init__(self, ip, mac, host = "Unknown"):
+        #Constructor with host name defaulted to Unknown in case it cannot be parsed
         self.hostName = host
         self.ipAddress = ip
         self.macAddress = mac
-		
-	def GetIp(self):
-		return self.ipAddress
-	
-	def GetMac(self):
-		return self.macAddress
+    def GetIp(self):
+	return (self.ipAddress)
+    def GetMac(self):
+	return (self.macAddress)
+    def GetHost(self):
+	return (self.hostName)
+    pass
