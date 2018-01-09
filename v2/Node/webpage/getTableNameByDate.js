195228eae0c6416e20526ffb2dde3beba0fdcaca
milorads <milorad.sekulovic@gmail.com>
2018-01-09 14:08:00 +0100

    added get table name by date function

---
+++ v2/Node/webpage/getTableNameByDate.js
@@ -0,0 +1,16 @@
+function getDateTableName()
+{
+	var today = new Date();
+	var dayString = "";
+	var monthString = "";
+	var yearString = Math.floor(today.getFullYear()%1000);
+	if(Math.floor(today.getDate()/10) == 0)
+	{
+		dayString = "0" + today.getDate();
+	}
+	if(Math.floor((today.getMonth()+1)/10) == 0)
+	{
+		monthString = "0" + (today.getMonth()+1);
+	}
+	return 'Td_m_Y'.replace('Y', yearString).replace('m', monthString).replace('d', dayString);
+}
