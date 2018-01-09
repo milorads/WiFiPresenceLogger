faf33dce5981a16c8b22a338788f3e60291c8c3a
milorads <milorad.sekulovic@gmail.com>
2018-01-09 12:12:21 +0100

    moving around

---
+++ v2/Node/webpage/registrationService.js
@@ -0,0 +1,67 @@
+var http = require('http');
+var express = require('express');
+var bodyParser = require('body-parser');
+var sqlite3 = require('sqlite3').verbose();
+var pdb = new sqlite3.Database('./PDB.db');
+var tdb = new sqlite3.Database('./TDB.db');
+
+var app = express();
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+app.use(express['static'](__dirname ));
+
+app.post('/registration', function(req, res) {
+  
+  console.log(req.body);
+  // db insert implementation here
+  var name = req.body.name;
+  var index = req.body.index;
+  var ip = req.body.ip;
+  var mac;
+  // select * from tdb where ip address = req's ip
+  //Perform SELECT Operation
+	tdb.all("SELECT * FROM connections WHERE ip="+ip+"ORDER BY Id ASC LIMIT 1",function(err,rows){
+		if (rows.length == 0) {
+			res.send('There was an error, please try again later.');
+		} else {
+			rows.forEach(function (row) {  
+            mac = row.Mac; 	
+			}) 
+			pdb.all("SELECT * FROM users where Mac="+mac,function(err,prows){
+				if(prows.length ==0){
+					// add to perm base and inform
+					pdb.run("INSERT into users(Mac, Index) VALUES ("+mac+", "+index+")");
+					res.send('You have successfully registered the index number "' + req.body.index + '".');
+				}
+				else{
+					var tempInd;
+								prows.forEach(function (prow) {tempInd = prow.Index;}) 
+					res.send('This device has already been registered for the following index: "'+tempInd+'".');
+					//inform that already registered
+				}
+			});
+		}
+	});
+  // if result found -> write down mac
+  //insert into pdb index, mac date etc.
+  
+});
+
+// Express route for any other unrecognised incoming requests
+app.get('*', function(req, res) {
+  res.status(404).send('Unrecognised API call');
+});
+
+// Express route to handle errors
+app.use(function(err, req, res, next) {
+  if (req.xhr) {
+    res.status(500).send('Oops, Something went wrong!');
+  } else {
+    next(err);
+  }
+});
+
+app.listen(3001);
