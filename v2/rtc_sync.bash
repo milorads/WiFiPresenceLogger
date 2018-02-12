518449e121cf0137cd81d2d4e684b7a1fd141f10
blukovac <lb130415d@student.etf.bg.ac.rs>
2018-02-12 13:07:54 +0100

    Sredjen bag sa setovanjem rtc-a

--- v2/rtc_sync.bash
+++ v2/rtc_sync.bash
@@ -3,32 +3,23 @@
 msgNoConnection="No connection to NTP time-server"
 msgConnection="Connection to NTP time-server"
 
-#provera da li ntpq radi
-if( ntpq -p | grep -q "^*" );then
+if (ntpq -p | grep -q "^*");then
 	#ima konekcije
 	echo $msgConnection
 	echo "-----------------"
-	#setovanje rtc registara preko hwclock-a 
-    echo ds3231 0x68 > /sys/class/i2c-adapter/i2c-0/new_device
-	
-	
-	hwclock -f /dev/rtc1 -w
-	
-	
-	#date +"%s" > i2cset 
-	#i2cset -y 0 0x68 0x00 $(date +"%S") b
-	#i2cset -y 0 0x68 0x01 $(date +"%M") b
-	#i2cset -y 0 0x68 0x02 $(date +"%H") b
-	#i2cset -y 0 0x68 0x04 $(date +"%d") b
-	#i2cset -y 0 0x68 0x05 $(date +"%m") b
-	#i2cset -y 0 0x68 0x06 $(date +"%y") b
-	#i2cset -y 0 0x68 0x03 $(date +"%u") b
-
+	#setovanje rtc registara
+	i2cset -y -f 0 0x68 0x00 0x$(date +"%S") b
+	i2cset -y -f 0 0x68 0x01 0x$(date +"%M") b
+	i2cset -y -f 0 0x68 0x02 0x$(date +"%H") b
+	i2cset -y -f 0 0x68 0x04 0x$(date +"%d") b
+	i2cset -y -f 0 0x68 0x05 0x$(date +"%m") b
+	i2cset -y -f 0 0x68 0x06 0x$(date +"%y") b
+	i2cset -y -f 0 0x68 0x03 0x$(date +"%u") b
 else
-	#nema konekcije, upisuje se sa rtc-a u sistemsko vreme
+	#nema konekcije
 	echo $msgNoConnection
-	i2cdump -r 0-6 -y -f 0 0x68 b | grep -A10 : > rtc_dump.txt
-	awk '{printf("20%s-%s-%s %s:%s:%s",$8,$7,$6,$4,$3,$2);}' < rtc_dump.txt > rtc_time.txt
-	date -s $(date +'%Y-%m-%dT%H:%M:%S' -f rtc_time.txt)
-	hwclock -s $(date +'%Y-%m-%dT%H:%M:%S' -f rtc_time.txt)
+	i2cdump -r 0-6 -y 0 0x68 b | grep -A10 : > /home/admin/WiFiPresenceLogger/v2/rtc_dump.txt
+	awk '{printf("20%s-%s-%s %s:%s:%s",$8,$7,$6,$4,$3,$2);}' < /home/admin/WiFiPresenceLogger/v2/rtc_dump.txt > rtc_time.txt
+	date -s $(date +'%Y-%m-%dT%H:%M:%S' -f /home/admin/WiFiPresenceLogger/v2/rtc_time.txt)
 fi
+
