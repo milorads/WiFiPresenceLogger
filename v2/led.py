ff8a44ae17f746f69f77020a673c146b21d9db00
Aleksa Brkić <aleksa.brkic12@gmail.com>
2018-08-21 10:38:31 +0200

    - novija verzija led.py modula

--- v2/led.py
+++ v2/led.py
@@ -9,21 +9,17 @@ R_PORTS = [13, 16]
 G_PORTS = [19, 20]
 B_PORTS = [26, 21]
 
-PWM_RANGE = 1800
-PERIOD_RANGE = 12
-
 def close():
 	for l in leds:
 		if l != None:
-			l._stop()
-			del l
+			l.close()
 
 class Led:
 	def __init__(self, index = 0):
 		if not is_ok_index(index):
 			raise ValueError('LED index out of range')
 		elif leds[index] != None:
-			raise ValueError('Indexed LED already exists. Use leds[] array to reference it');
+			self = leds[index]
 		else:
 			self.index = index
 			self._init_ports()
@@ -50,10 +46,10 @@ class Led:
 		self.g = G_PORTS[self.index]
 		self.b = B_PORTS[self.index]
 	
-	def on(self, color, intensity = 100):
+	def on(self, color = 'green', intensity = 40):
 		if not is_ok_color(color):
 			raise ValueError('Color name not recognized')
-		elif not (0 <= intensity and intensity <= 100):
+		elif not is_ok_intensity(intensity):
 			raise ValueError('Intensity out of range')
 		else:
 			self._stop()
@@ -62,12 +58,12 @@ class Led:
 	def off(self):
 		self._stop()
 	
-	def blink(self, color, freq, intensity = 100):
+	def blink(self, color = 'green', freq = 1, intensity = 40):
 		if not is_ok_color(color):
 			raise ValueError('Color name not recognized')
-		elif not (freq > 0):
+		elif not is_ok_frequency(freq):
 			raise ValueError('Frequency out of range')
-		elif not (0 <= intensity and intensity <= 100):
+		elif not is_ok_intensity(intensity):
 			raise ValueError('Intensity out of range')
 		else:
 			self._stop()
@@ -78,39 +74,35 @@ class Led:
 	def is_off(self):
 		return not self.is_on()
 	
-	def _activate(self, color, intensity = 100):
+	def _activate(self, color, intensity):
 		self._active_thread = Thread(target = Led._activate_thread,
 				args = (self, color, intensity, ))
 		self._active_flag = True
 		self._active_thread.start()
 	
 	def _activate_thread(self, color, intensity):
-		step = 1.0 * PWM_RANGE / (intensity * (PWM_RANGE / 100))
-		steps = step
-		cnt = 0
-		while self._active_flag:
-			if cnt >= steps:
-				val = 1
-				steps = steps + step
-			else:
-				val = 0
-			if cnt >= PWM_RANGE:
-				cnt = 0
-				steps = step
-			else:
-				cnt = cnt + 1
-			
-			sz = len(self.color_ports(color))
-			for i in range(0, sz):
-				port = self.color_ports(color)[i]
-				range_chunk = PERIOD_RANGE / sz
-				if i * range_chunk <= (cnt % PERIOD_RANGE) and (cnt % PERIOD_RANGE) < (i + 1) * range_chunk:
-					GPIO.output(port, val)
-				else:
-					GPIO.output(port, 0)
+		step = 100.0 / intensity
+		cnt = 0.0
+		color_cnt = 0
 		
-		for port in self.color_ports(color):
-			GPIO.output(port, 0)
+		ports = self.color_ports(color)
+		sz = len(ports)
+		
+		while self._active_flag:
+			if color_cnt == 0:
+				if cnt >= step:
+					val = 1
+					cnt = cnt - step
+				else:
+					val = 0
+				cnt = cnt + 1
+			GPIO.output(ports[(color_cnt - 1) % sz], 0)
+			GPIO.output(ports[color_cnt], val)
+			color_cnt = (color_cnt + 1) % sz
+			time.sleep(0.001)
+		
+		for p in ports:
+			GPIO.output(p, 0)
 	
 	def _deactivate(self):
 		if self._active_flag:
@@ -130,6 +122,7 @@ class Led:
 			time.sleep(t)
 			self._deactivate()
 			time.sleep(t)
+		self._deactivate()
 	
 	def _deactivate_blinking(self):
 		if self._blink_flag == True:
@@ -139,36 +132,42 @@ class Led:
 	def _stop(self):
 		self._deactivate_blinking()
 		self._deactivate()
+		for p in [self.r, self.g, self.b]:
+			GPIO.output(p, 0)
 	
 	def color_ports(self, c):
-		if c == 'red':
+		if c == 'red' or c == 'error':
 			return [self.r]
-		elif c == 'green':
+		elif c == 'green' or c == 'success':
 			return [self.g]
 		elif c == 'blue':
 			return [self.b]
-		elif c == 'yellow':
+		elif c == 'yellow' or c == 'warning':
 			return [self.r, self.g]
 		elif c == 'purple':
 			return [self.r, self.b]
 		elif c == 'cyan':
 			return [self.g, self.b]
-		elif c == 'white':
+		elif c == 'white' or c == 'blank':
 			return [self.r, self.g, self.b]
 		else:
 			return None
 
-def is_ok_index(index):
-	return 0 <= index and index < NUMBER_OF_LEDS
+def is_ok_index(i):
+	return 0 <= i and i < NUMBER_OF_LEDS
+def is_ok_intensity(i):
+	return 0 <= i and i <= 100
+def is_ok_frequency(f):
+	return f > 0 and f <= 100
 
-def has_r(color):
-	return color == 'red' or color == 'yellow' or color == 'purple' or color == 'white'
+def has_r(c):
+	return c == 'red' or c == 'yellow' or c == 'purple' or c == 'white'
 
-def has_g(color):
-	return color == 'green' or color == 'yellow' or color == 'cyan' or color == 'white'
+def has_g(c):
+	return c == 'green' or c == 'yellow' or c == 'cyan' or c == 'white'
 
-def has_b(color):
-	return color == 'blue' or color == 'purple' or color == 'cyan' or color == 'white'
+def has_b(c):
+	return c == 'blue' or c == 'purple' or c == 'cyan' or c == 'white'
 
-def is_ok_color(color):
-	return has_r(color) or has_g(color) or has_b(color)
+def is_ok_color(c):
+	return has_r(c) or has_g(c) or has_b(c)
