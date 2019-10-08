afdb2fb54bf16c84a65e6af246964913a30b7d6f
Aleksa Brkić <aleksa.brkic12@gmail.com>
2019-10-08 11:46:12 +0200

    run.py - refactor folders shutdown_time.txt moved to Temp folder

--- v3/Py/run.py
+++ v3/Py/run.py
@@ -3,10 +3,14 @@ import time
 import subprocess
 import mysql.connector
 
-cur_dir = os.path.dirname(os.path.realpath(__file__))
+py_dir = os.path.dirname(os.path.realpath(__file__)) + '/'
+bash_dir = cur_dir + '../Bash/'
+c_dir = cur_dir + '../C/'
+temp_dir = cur_dir + '/../Temp'
+
 #TODO: add temp dir and redirect temps to that dir
-subprocess.call(['sudo', cur_dir + '/ledon', '0', 'y'])
-subprocess.call(['sudo', 'bash', cur_dir + '/wait_for_mysql.bash'])#TODO: this will break since sys_time is in bash/wait_for_mysql
+subprocess.call(['sudo', c_dir + 'ledon', '0', 'y'])
+subprocess.call(['sudo', 'bash', bash_dir + 'wait_for_mysql.bash'])#TODO: this will break since sys_time is in bash/wait_for_mysql
 
 db = mysql.connector.connect(
 	host = 'localhost',
@@ -22,30 +26,30 @@ def db_end_all_logs(end_time):
 	db.commit()
 	print "upisivanje izlaza"
 
-file = open(cur_dir + "/shutdown_time.txt", "r")
+file = open(temp_dir + "shutdown_time.txt", "r")
 shutdown_time = file.readline()
 
 if shutdown_time != "":
 	print shutdown_time
 	db_end_all_logs(shutdown_time)
-	subprocess.call(['sudo', 'bash', cur_dir + '/sys_time.bash', '3'])#TODO: this will break since sys_time is in bash/sys_time
+	subprocess.call(['sudo', 'bash', bash_dir + 'sys_time.bash', '3'])#TODO: this will break since sys_time is in bash/sys_time
 
 prev_time = ""
 cur_time = ""
 file.close()
 
-subprocess.call(['sudo', cur_dir + '/ledon', '0', 'g'])
-subprocess.Popen(['sudo', 'bash', cur_dir + '/check_connection.bash'])#TODO: this will break since sys_time is in bash/wait_for_mysql
+subprocess.call(['sudo', c_dir + 'ledon', '0', 'g'])
+subprocess.Popen(['sudo', 'bash', bash_dir + 'check_connection.bash'])#TODO: this will break since sys_time is in bash/wait_for_mysql
 
 while True:
 	#######################
 	cur_time = subprocess.check_output('sudo date "+%d"', shell=True)
 	if ((cur_time != prev_time) and (prev_time != "")):
-		file = open(cur_dir + "/shutdown_time.txt", "r")
+		file = open(temp_dir + "shutdown_time.txt", "r")
 		shutdown_time  =  file.readline()
 		db_end_all_logs(shutdown_time)
 	prev_time = subprocess.check_output('sudo date "+%d"', shell=True)
 	#######################
-	subprocess.call('sudo date "+%Y-%m-%d %T.%2N" > ' + cur_dir + '/shutdown_time.txt', shell=True)
-	subprocess.call(['sudo', 'python', cur_dir + '/station_check_instance.py'])
+	subprocess.call('sudo date "+%Y-%m-%d %T.%2N" > ' + temp_dir + 'shutdown_time.txt', shell=True)
+	subprocess.call(['sudo', 'python', py_dir + 'station_check_instance.py'])
 	time.sleep(30)
